f90f2f6510ff683056deb4ab8ab0a63f3724a085
AkashAhilan <173340746+AkashAhilan@users.noreply.github.com>
2025-07-25 13:13:26 -0400

    Complete the First 9 JavaScript Questions
    The first questions were answered within one and a
    half days ther should be another commit in an hour or so
    which includes the rest of the solutions.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, removeValue) {
+  return array.filter((item) => item != removeValue);
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
-
+const sumAll = function (numOne, numTwo) {
+  let length = numTwo - numOne;
+  let large;
+  let small;
+  let sum = 0;
+  if (
+    !Number.isInteger(numOne) ||
+    !Number.isInteger(numTwo) ||
+    numOne < 0 ||
+    numTwo < 0
+  ) {
+    return "ERROR";
+  } else if (numOne > numTwo) {
+    large = numOne;
+    small = numTwo;
+  } else {
+    small = numOne;
+    large = numTwo;
+  }
+  length = large - small;
+  if (length == 0) {
+    return "ERROR";
+  }
+  for (let i = small; small <= large; small++) {
+    sum += small;
+  }
+  return sum;
 };
 
 // Do not edit below this line
